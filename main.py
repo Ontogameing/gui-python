58c0a7c9d90ba6bc44427426984a6be8c3c959d1
Ontogameing <karimkhaniabraham@gmail.com>
2022-08-19 16:55:31 -0400

    home label

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from tkinter import *
 
+HOMEBGCOLOR = "black"
+
 root = Tk()
+root.config(bg = HOMEBGCOLOR)
+
+homeLabel = Label(root, text = "Home", bg = HOMEBGCOLOR, fg = "white").pack()
 
 root.mainloop()
